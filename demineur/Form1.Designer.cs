b7f375401ec7596765a2b457a3914c0e6990e76b
Arshess Ney <arshess@hotmail.fr>
2016-02-23 13:47:56 +0100

    v1.0

---
+++ demineur/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace demineur
+{
+    partial class demineur
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Tb_nbr_mine = new System.Windows.Forms.TextBox();
+            this.btn_fire = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.table1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lb_nb_bombe = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre Mine : ";
+            // 
+            // Tb_nbr_mine
+            // 
+            this.Tb_nbr_mine.Location = new System.Drawing.Point(6, 25);
+            this.Tb_nbr_mine.Name = "Tb_nbr_mine";
+            this.Tb_nbr_mine.Size = new System.Drawing.Size(100, 20);
+            this.Tb_nbr_mine.TabIndex = 2;
+            // 
+            // btn_fire
+            // 
+            this.btn_fire.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_fire.Location = new System.Drawing.Point(6, 58);
+            this.btn_fire.Name = "btn_fire";
+            this.btn_fire.Size = new System.Drawing.Size(100, 23);
+            this.btn_fire.TabIndex = 4;
+            this.btn_fire.Text = "A L\'ASSAUT !!";
+            this.btn_fire.UseVisualStyleBackColor = true;
+            this.btn_fire.Click += new System.EventHandler(this.btn_fire_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btn_fire);
+            this.panel1.Controls.Add(this.Tb_nbr_mine);
+            this.panel1.Location = new System.Drawing.Point(368, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(116, 90);
+            this.panel1.TabIndex = 6;
+            // 
+            // table1
+            // 
+            this.table1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.table1.ColumnCount = 10;
+            this.table1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.table1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.table1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.table1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.table1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.table1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.table1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.table1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.table1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.table1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.table1.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
+            this.table1.Location = new System.Drawing.Point(12, 12);
+            this.table1.Name = "table1";
+            this.table1.RowCount = 10;
+            this.table1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.table1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.table1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.table1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.table1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.table1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.table1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.table1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.table1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.table1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.table1.Size = new System.Drawing.Size(334, 284);
+            this.table1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(356, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Bien joué, tu retentes ? ";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(365, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "bombes restante :";
+            this.label3.Visible = false;
+            // 
+            // lb_nb_bombe
+            // 
+            this.lb_nb_bombe.AutoSize = true;
+            this.lb_nb_bombe.Location = new System.Drawing.Point(462, 140);
+            this.lb_nb_bombe.Name = "lb_nb_bombe";
+            this.lb_nb_bombe.Size = new System.Drawing.Size(19, 13);
+            this.lb_nb_bombe.TabIndex = 11;
+            this.lb_nb_bombe.Text = "10";
+            this.lb_nb_bombe.Visible = false;
+            // 
+            // demineur
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(500, 311);
+            this.Controls.Add(this.lb_nb_bombe);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.table1);
+            this.Controls.Add(this.panel1);
+            this.Name = "demineur";
+            this.Text = "Arshess";
+            this.Load += new System.EventHandler(this.Arshess_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Tb_nbr_mine;
+        private System.Windows.Forms.Button btn_fire;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel table1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lb_nb_bombe;
+    }
+}
---
+++ demineur/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace demineur
+{
+    public partial class demineur : Form
+    {
+        int nbr_mine = 0, x = 0, y = 0, bomba = 0, desamorce=0, antminerestante=0;
+        // matrice de 9 sur 9
+        string[,] carte = new string[10, 10];
+        bool loose = false;
+        public demineur()
+        {
+            InitializeComponent();
+        }
+        private void gengrille()
+        {
+            for (int x = 0; x < 10; x++)
+                for (int y = 0; y < 10; y++)
+                {
+                    Button bt = new Button();
+                    bt.Name = "bt-" + x + "-" + y;
+                    bt.MouseDown += Bt_Click;
+                    table1.Controls.Add(bt, x, y);
+                }
+            table1.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+        }
+
+        private void perdu(object sender)
+        {
+            Button bt = sender as Button;
+            MessageBox.Show("et hop, une jambe de moins", "Perdu");
+            table1.Enabled = false;
+            btn_fire.Enabled = true;
+            label2.Visible = true;
+            bt.Text = "X";
+            loose = true;
+
+            //---------------------------------------------------
+            //on affiche un X sur chaque bombe de la map
+            //---------------------------------------------------
+            Button b;
+            for (int x = 0; x < 10; x++)
+                for (int y = 0; y < 10; y++)
+                {
+                    if (carte[x, y] == "X")
+                    {
+                        b = table1.GetControlFromPosition(x, y) as Button;
+                        b.Text = "X";
+                    }
+                }           
+            
+        }
+        private void Arshess_Load(object sender, EventArgs e)
+        {
+            gengrille();
+            table1.Enabled = false;
+        }
+
+        private void verif_cases(object sender, int xx, int yy,Button bt)
+        {
+            // les verticales ou l'inverse j'ai un doute
+            if (xx < 9)
+            {
+                if (carte[xx + 1, yy] == "X")
+                    bomba++;
+            }
+            if (xx > 0)
+            {
+                if (carte[xx - 1, yy] == "X")
+                    bomba++;
+            }
+            // les horizontales  ou l'inverse j'ai un doute
+
+            if (yy < 9)
+            {
+                if (carte[xx, yy + 1] == "X")
+                    bomba++;
+            }
+            if (yy > 0)
+            {
+                if (carte[xx, yy - 1] == "X")
+                    bomba++;
+            }
+            //les diagonales, ça c'est sûr
+            if (xx < 9 && yy < 9)
+            {
+                if (carte[xx + 1, yy + 1] == "X")
+                    bomba++;
+            }
+            if (xx > 0 && yy > 0)
+            {
+                if (carte[xx - 1, yy - 1] == "X")
+                    bomba++;
+            }
+            if (xx < 9 && yy > 0)
+            {
+                if (carte[xx + 1, yy - 1] == "X")
+                    bomba++;
+            }
+            if (xx > 0 && yy < 9)
+            {
+                if (carte[xx - 1, yy + 1] == "X")
+                    bomba++;
+            }
+            bt.Text = bomba.ToString();
+           
+            bomba = 0;
+
+        }
+        private void Bt_Click(object sender, MouseEventArgs e)
+        {
+            Button bt = sender as Button;
+            string[] test = bt.Name.Split('-');
+            int xx = Convert.ToInt32(test[1]);
+            int yy = Convert.ToInt32(test[2]);
+            if (e.Button == MouseButtons.Left)
+            {
+                //Console.WriteLine("{0} {1}", test[1], test[2]);
+                if (carte[xx, yy] == "X")
+                { 
+                    perdu(sender);
+                }
+                else
+                {
+                    verif_cases(sender,xx,yy,bt);
+                }
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                if (bt.Text == "M")
+                {
+                    bt.Text = "";
+                    antminerestante++;
+                }
+                else
+                {
+                    if (carte[xx, yy] == "X" && antminerestante > 0)
+                    {
+                        bt.Text = "M";
+                        desamorce++;
+                        antminerestante--;
+                    }
+                    else
+                    {
+                        bt.Text = "M";
+                        antminerestante--;
+                    }
+
+                    if (desamorce == nbr_mine)
+                    {
+                        label2.Text = "You are Winner ";
+                        label2.Visible = true;
+                        table1.Enabled = false;
+                        btn_fire.Enabled = true;
+                        loose = true;
+                    }
+                }
+                lb_nb_bombe.Text = antminerestante.ToString();
+            }
+        }
+        private void placement()
+        {
+            Random pos = new Random();
+            for (int i = 0; i < nbr_mine; i++)
+            {
+                do // tant que la case au rand contient dejà une mine on reroll
+                {
+                    x = pos.Next(0, 10);
+                    y = pos.Next(0, 10);
+                } while (carte[x, y] == "X");
+
+                carte[x, y] = "X";
+                //Console.WriteLine("x = {0} y = {1}",x,y);
+            }
+        }
+
+        private void btn_fire_Click(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch(Tb_nbr_mine.Text, "^[0-9]{1,2}$"))
+            {
+                Array.Clear(carte, 0, carte.Length);
+                nbr_mine = Convert.ToInt32(Tb_nbr_mine.Text);
+                placement();
+                btn_fire.Enabled = false;
+                table1.Enabled = true;
+                if (loose)
+                {
+                    table1.Controls.Clear();
+                    gengrille();
+                }
+                antminerestante = nbr_mine;
+                lb_nb_bombe.Visible = true;
+                label3.Visible = true;
+                lb_nb_bombe.Text = Tb_nbr_mine.Text;
+                label2.Visible = false;
+            }
+        }
+
+    }    
+}
